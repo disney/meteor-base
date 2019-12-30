478743db8a170bc7b265de4fc18f50876d293021
Geoffrey Booth <webmaster@geoffreybooth.com>
2019-12-30 00:37:28 -0800

    Poll without p-wait-for and promises, improving likelihood of exiting the process on success

--- src/docker/entrypoint.sh
+++ src/docker/entrypoint.sh
@@ -7,15 +7,17 @@ cd $SCRIPTS_FOLDER
 # Poll until we can successfully connect to MongoDB
 echo 'Connecting to MongoDB...'
 node <<- 'EOJS'
-require('p-wait-for')(function() {
-	return new Promise(function (resolve) {
-		require('mongodb').MongoClient.connect(process.env.MONGO_URL, function(err, client) {
-			const successfullyConnected = err == null;
-			if (successfullyConnected) {
-				client.close();
-			}
-			resolve(successfullyConnected);
-		});
+const mongoClient = require('mongodb').MongoClient;
+setInterval(function() {
+	mongoClient.connect(process.env.MONGO_URL, function(err, client) {
+		if (client) {
+			client.close();
+		}
+		if (err) {
+			console.error(err);
+		} else {
+			process.exit(0);
+		}
 	});
 }, 1000);
 EOJS
