78f9f8f6e4bebe95887221ffc4b40ab25a065862
Geoffrey Booth <webmaster@geoffreybooth.com>
2020-03-17 15:05:07 -0700

    Move Mongo connection test script into its own file

---
+++ src/docker/connect-to-mongo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -o errexit
+
+cd $SCRIPTS_FOLDER
+
+# Poll until we can successfully connect to MongoDB
+echo 'Connecting to MongoDB...'
+node <<- 'EOJS'
+const mongoClient = require('mongodb').MongoClient;
+setInterval(function() {
+	mongoClient.connect(process.env.MONGO_URL, function(err, client) {
+		if (client) {
+			client.close();
+		}
+		if (err) {
+			console.error(err);
+		} else {
+			process.exit(0);
+		}
+	});
+}, 1000);
+EOJS
--- src/docker/entrypoint.sh
+++ src/docker/entrypoint.sh
@@ -5,22 +5,7 @@ set -o errexit
 cd $SCRIPTS_FOLDER
 
 # Poll until we can successfully connect to MongoDB
-echo 'Connecting to MongoDB...'
-node <<- 'EOJS'
-const mongoClient = require('mongodb').MongoClient;
-setInterval(function() {
-	mongoClient.connect(process.env.MONGO_URL, function(err, client) {
-		if (client) {
-			client.close();
-		}
-		if (err) {
-			console.error(err);
-		} else {
-			process.exit(0);
-		}
-	});
-}, 1000);
-EOJS
+source ./connect-to-mongo.sh
 
 echo 'Starting app...'
 cd $APP_BUNDLE_FOLDER/bundle
