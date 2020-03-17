b268caaaa975e974fa81f0863fdfb5c902e188dd
Geoffrey Booth <webmaster@geoffreybooth.com>
2020-03-17 15:05:34 -0700

    Provide a hook for running commands on startup if a child script desires

--- src/docker/entrypoint.sh
+++ src/docker/entrypoint.sh
@@ -4,6 +4,11 @@ set -o errexit
 
 cd $SCRIPTS_FOLDER
 
+# Source an init script that a child image may have added
+if [ -x ./startup.sh ]; then
+	source ./startup.sh
+fi
+
 # Poll until we can successfully connect to MongoDB
 source ./connect-to-mongo.sh
 
