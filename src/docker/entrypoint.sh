2e429ae76e5803a146fd5406de517ed7c4b758c1
Jesse Rosenberger <git@jro.cc>
2020-04-03 15:13:33 +0300

    Skip waiting for MongoDB if not installed or configured.
    MongoDB is no longer an absolute requirement of all Meteor
    applications and can be optionally removed.
    
    This adds a guard that skips the polling and waiting for
    Mongo to become available if either the 'mongodb' module
    is not present, or if the 'MONGO_URL' environment variable
    is unset.

--- src/docker/entrypoint.sh
+++ src/docker/entrypoint.sh
@@ -7,6 +7,12 @@ cd $SCRIPTS_FOLDER
 # Poll until we can successfully connect to MongoDB
 echo 'Connecting to MongoDB...'
 node <<- 'EOJS'
+// If mongodb doesn't exist do not attempt waiting for it.
+try { require.resolve('mongodb'); } catch { process.exit(0); };
+
+// Similarly, if MONGO_URL is not set, do not wait its startup.
+if (typeof process.env.MONGO_URL === 'undefined') process.exit(0);
+
 const mongoClient = require('mongodb').MongoClient;
 setInterval(function() {
 	mongoClient.connect(process.env.MONGO_URL, function(err, client) {
