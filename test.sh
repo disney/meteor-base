6ec9eeac2e3c33517ccc406bf6ca5e165b77322a
chneau <charles63500@gmail.com>
2021-07-14 22:37:52 +0100

    Actually 12.22.3 is the latest for node_version 12.22

--- test.sh
+++ test.sh
@@ -60,9 +60,9 @@ for version in "${meteor_versions[@]}"; do
 	if [[ "${version}" == 1.6* ]] || [[ "${version}" == 1.7* ]] || [[ "${version}" == 1.8* ]]; then
 		node_version='8.17.0'
 
-	# Versions 1.9 through 2.2.x need Node 12.22.2
+	# Versions 1.9 through 2.2.x need Node 12.22.3
 	elif [[ "${version}" == 1.9* ]] || [[ "${version}" == 1.10* ]] || [[ "${version}" == 1.11* ]] || [[ "${version}" == 1.12* ]] || [[ "${version}" == 2.0* ]] || [[ "${version}" == 2.1* ]] || [[ "${version}" == 2.2* ]]; then
-		node_version='12.22.2'
+		node_version='12.22.3'
 
 	# Versions >= 2.3 need Node 14.17.3
 	else
