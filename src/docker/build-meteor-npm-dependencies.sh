8511aeca00753f39ce6e39848da3445797c285a5
Nathan Carlson <nathan@caucusroom.com>
2022-02-02 17:18:49 -0700

    feat: replace hash npm uses

--- src/docker/build-meteor-npm-dependencies.sh
+++ src/docker/build-meteor-npm-dependencies.sh
@@ -4,17 +4,11 @@ set -o errexit
 
 printf "\n[-] Installing Meteor application server NPM dependencies...\n\n"
 
-if hash npm 2>/dev/null; then
-	npm_cmd='npm'
-else
-	npm_cmd='meteor npm'
-fi
-
 cd $APP_BUNDLE_FOLDER/bundle/programs/server/
-$npm_cmd install
+npm install
 
 if [[ "$1" = '--build-from-source' ]]; then
-	$npm_cmd rebuild --build-from-source
+	npm rebuild --build-from-source
 	cd $APP_BUNDLE_FOLDER/bundle/programs/server/npm
-	$npm_cmd rebuild --build-from-source
+	npm rebuild --build-from-source
 fi
