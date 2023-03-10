f5e019c95502d1770467513b653f1878a3dd3196
Iván Cabrera <diavrank@Ivans-MacBook-Pro.local>
2023-03-09 18:11:55 -0600

    Feat: Add 2.11.0

--- support.sh
+++ support.sh
@@ -58,5 +58,6 @@ set_node_version() {
 	elif [[ "$1" == 2.9.0 ]]; then node_version='14.21.1'
 	elif [[ "$1" == 2.9.1 ]]; then node_version='14.21.2'
 	elif [[ "$1" == 2.10.0 ]]; then node_version='14.21.2'
+	elif [[ "$1" == 2.11.0 ]]; then node_version='14.21.3'
 	fi # End of versions
 }
